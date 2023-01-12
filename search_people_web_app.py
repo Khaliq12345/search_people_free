66908cc50353986814a2ca416d1e1b63b890a163
Khaliq Salawou <khaliqsalawoudeen@gmail.com>
2023-01-12 09:28:52 +0000

    Update search_people_web_app.py

--- search_people_web_app.py
+++ search_people_web_app.py
@@ -17,11 +17,6 @@ footer {
 st.markdown(hide_menu, unsafe_allow_html=True)
 
 def format_text(text):
-    if text == np.nan:
-        text = df['State'].value_counts().idxmax()
-    else:
-        pass
-    
     text = text.lower()
 
     # Replace spaces with hyphens
@@ -122,8 +117,8 @@ if uploaded_file is not None:
     mn = df['Middle Name'].to_list()
     cities = df['City'].to_list()
     states = df['State'].to_list()
-    city = [df['State'].value_counts().idxmax() if x is np.nan else x for x in cities]
-    state = [df['State'].value_counts().idxmax() if x is np.nan else x for x in states]
+    city = [x = df['State'].value_counts().idxmax() if x is np.nan else x for x in cities]
+    state = [x = df['State'].value_counts().idxmax() if x is np.nan else x for x in states]
 
 button = st.button('Scrape!')
 
